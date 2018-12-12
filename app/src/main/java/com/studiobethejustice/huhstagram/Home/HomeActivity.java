50decd6ddc8a792918ad1830b644c913ae846d61
Theodore <bethejustice@naver.com>
2018-12-13 02:03:01 +0900

    feature: firebase auth login function

--- app/src/main/java/com/studiobethejustice/huhstagram/Home/HomeActivity.java
+++ app/src/main/java/com/studiobethejustice/huhstagram/Home/HomeActivity.java
@@ -1,19 +1,24 @@
 package com.studiobethejustice.huhstagram.Home;
 
 import android.content.Context;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.ittianyu.bottomnavigationviewex.BottomNavigationViewEx;
 import com.nostra13.universalimageloader.core.ImageLoader;
+import com.studiobethejustice.huhstagram.Login.LoginActivity;
 import com.studiobethejustice.huhstagram.R;
-import com.studiobethejustice.huhstagram.Utils.SectionPagerAdapter;
 import com.studiobethejustice.huhstagram.Utils.BottomNavigationViewHelper;
+import com.studiobethejustice.huhstagram.Utils.SectionPagerAdapter;
 import com.studiobethejustice.huhstagram.Utils.UniversalImageLoader;
 
 public class HomeActivity extends AppCompatActivity {
@@ -22,27 +27,28 @@ public class HomeActivity extends AppCompatActivity {
 
     private Context mContext = HomeActivity.this;
 
+    //Firebase
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
         Log.d(TAG, "onCreate: starting.");
 
+        setupFirebaseAuth();
+
         initImageLoader();
         setupBottomNavigationView();
         setupViewPager();
-    }
-
-    private void initImageLoader(){
-        UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
-        ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
 
     /**
      * Responsible for adding the 3 tabs: Camera, Home, Messages
      */
 
-    private void setupViewPager(){
+    private void setupViewPager() {
         SectionPagerAdapter adapter = new SectionPagerAdapter(getSupportFragmentManager());
         adapter.addFragment(new CameraFragment());
         adapter.addFragment(new HomeFragment());
@@ -58,9 +64,11 @@ public class HomeActivity extends AppCompatActivity {
         tabLayout.getTabAt(2).setIcon(R.drawable.ic_message);
     }
 
+
     /**
      * BottomNavigationView setup
      */
+
     private void setupBottomNavigationView() {
         Log.d(TAG, "setupBottomNavigationView: setting up BottomNavigationView");
         BottomNavigationViewEx bottomNavigationViewEx = findViewById(R.id.bottomNavViewBar);
@@ -69,5 +77,59 @@ public class HomeActivity extends AppCompatActivity {
         Menu menu = bottomNavigationViewEx.getMenu();
         MenuItem menuItem = menu.getItem(ACTIVITY_NUM);
         menuItem.setChecked(true);
+    }
+
+
+    /**
+     * setup the firebase auto object
+     */
+
+    private void setupFirebaseAuth() {
+        Log.d(TAG, "setupFirebaseAuth: setting up firebase auth.");
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                // check if the user is logged in
+                CheckCurrentUser(user);
+
+                if (user != null) {
+                    Log.d(TAG, "onAuthStateChanged: signed_in" + user.getUid());
+                } else {
+                    Log.d(TAG, "onAuthStateChanged: signed_out");
+                }
+            }
+        };
+    }
+
+    private void CheckCurrentUser(FirebaseUser user){
+        Log.d(TAG, "CheckCurrentUser: checking if user is logged in.");
+
+        if(user == null){
+            Intent intent = new Intent(mContext, LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+        CheckCurrentUser(mAuth.getCurrentUser());
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+
+    private void initImageLoader() {
+        UniversalImageLoader universalImageLoader = new UniversalImageLoader(mContext);
+        ImageLoader.getInstance().init(universalImageLoader.getConfig());
     }
 }
